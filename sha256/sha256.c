25e4d9366049efcca04aaf14acf7e273a6756dd2
Boris Nagaev <bnagaev@gmail.com>
2014-10-24 21:55:44 +0400

    Revert "move contents of sha256.h into sha256.c"
    This reverts commit 800a8845471a98026723e52a6055ba15e1473f0f.

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -20,25 +20,7 @@
 
 #include <string.h>
 
-#ifndef uint8
-#define uint8  unsigned char
-#endif
-
-#ifndef uint32
-#define uint32 unsigned long int
-#endif
-
-typedef struct
-{
-    uint32 total[2];
-    uint32 state[8];
-    uint8 buffer[64];
-}
-sha256_context;
-
-void sha256_starts( sha256_context *ctx );
-void sha256_update( sha256_context *ctx, uint8 *input, uint32 length );
-void sha256_finish( sha256_context *ctx, uint8 digest[32] );
+#include "sha256.h"
 
 #define GET_UINT32(n,b,i)                       \
 {                                               \
