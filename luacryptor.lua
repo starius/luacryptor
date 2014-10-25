1010cdd1a8c175e68694dd57c2b8f2c6622b4c18
Boris Nagaev <bnagaev@gmail.com>
2014-10-25 17:26:08 +0400

    split long lines of source

--- luacryptor.lua
+++ luacryptor.lua
@@ -2,9 +2,12 @@
 local m = {}
 
 function m.cleanSource(src)
-    src = src:gsub("function ([%w_%.]+):([%w_]+)%(%)", "%1.%2 = function(self)")
-    src = src:gsub("function ([%w_%.]+):([%w_]+)%(", "%1.%2 = function(self,")
-    src = src:gsub("function ([%w_%.]+)%(", "%1 = function(")
+    src = src:gsub("function ([%w_%.]+):([%w_]+)%(%)",
+        "%1.%2 = function(self)")
+    src = src:gsub("function ([%w_%.]+):([%w_]+)%(",
+        "%1.%2 = function(self,")
+    src = src:gsub("function ([%w_%.]+)%(",
+        "%1 = function(")
     return src
 end
 
